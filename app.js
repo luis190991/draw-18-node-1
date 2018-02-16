63ed8f00c37f1efe3914be67fb504a1f406a4846
Luis Antonio Ramirez Martinez <luis@blacklabs.mx>
2018-02-16 08:28:27 -0700

    Se agrega funcionalidad básica del servidor de node.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+http.createServer((request, response) => {
+  fs.readFile('./index.html', (err, html) => {
+    logger.info("Conexón a la aplicación.");
+    if (err) {
+      logger.warn('404 - not found');
+      response.writeHead(404, {
+        'Content-Type': 'text/html'
+      });
+      response.write("404 - not found");
+      response.end();
+    } else {
+      logger.info('200 - ok');
+      response.write(html);
+      response.end();
+    }
+  });
+}).listen(3000);
+
+
+
+/*let server = function(){
+
+};
+http.createServer(server);
+
+
+http.createServer(function(){
+
+});*/
